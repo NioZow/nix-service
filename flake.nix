09e15f33d81f2a4a9d5da9bb8263333823eb680b
NioZow <niozow@proton.me>
2026-09-12 23:00:00 +0200

    chore: drop unused home-manager input

--- flake.nix
+++ flake.nix
@@ -3,16 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    home-manager,
   }: let
     lib = nixpkgs.lib;
     systems = [
